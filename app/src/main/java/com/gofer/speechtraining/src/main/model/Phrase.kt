2099b91952cdacf15ab18767d4f4ee916fa05725
Krzysztof Hofman <Krzysztof.Hofman@tomtom.com>
2024-07-29 22:39:38 +0200

    parcelize Phrase class

--- app/src/main/java/com/gofer/speechtraining/src/main/model/Phrase.kt
+++ app/src/main/java/com/gofer/speechtraining/src/main/model/Phrase.kt
@@ -1,11 +1,36 @@
 package com.gofer.speechtraining.src.main.model
 
-data class Phrase(val name: String = "", var isSelected: Boolean = false) {
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Phrase(val name: String = "", var isSelected: Boolean = false) : Parcelable {
+
+  constructor(parcel: Parcel) : this(
+    parcel.readString().orEmpty(),
+    parcel.readBoolean()
+  )
+
   fun toggle() {
     isSelected = isSelected.not()
   }
 
-  fun selected(): Boolean {
-    return isSelected == true
+  override fun writeToParcel(parcel: Parcel, flags: Int) {
+    parcel.writeString(name)
+    parcel.writeByte(if (isSelected) 1 else 0)
   }
+
+  override fun describeContents(): Int {
+    return 0
+  }
+
+  companion object CREATOR : Parcelable.Creator<Phrase> {
+    override fun createFromParcel(parcel: Parcel): Phrase {
+      return Phrase(parcel)
+    }
+
+    override fun newArray(size: Int): Array<Phrase?> {
+      return arrayOfNulls(size)
+    }
+  }
+
 }
