d6b2e4ebec36a26da37f535f3cf9833358d04d17
Krzysztof Hofman <Krzysztof.Hofman@tomtom.com>
2024-08-15 14:09:14 +0200

    Fix TTS speak

--- app/src/main/java/com/gofer/speechtraining/src/main/model/SpeechTrainingDataViewModel.kt
+++ app/src/main/java/com/gofer/speechtraining/src/main/model/SpeechTrainingDataViewModel.kt
@@ -3,13 +3,7 @@ package com.gofer.speechtraining.src.main.model
 import androidx.lifecycle.ViewModel
 
 class SpeechTrainingDataViewModel (var data: SpeechTrainingData): ViewModel() {
-  fun addTrainingTopic(trainingItem: SpeechTrainingItem) {
-    data.addTrainingItem(trainingItem);
-  }
 
-  fun addTrainingTopicNames() = data.addTrainingTopicNames()
-
-  fun addTrainingTopics() = data.addTopic()
   fun getTrainingTopics() = data.getTrainingTopics()
 
   fun getTrainingPhrases(trainingId: Long) = data.getTrainingPhrases(trainingId)
--- app/src/main/java/com/gofer/speechtraining/ui/theme/Color.kt
+++ app/src/main/java/com/gofer/speechtraining/ui/theme/Color.kt
@@ -2,15 +2,6 @@ package com.gofer.speechtraining.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// define your colors for dark theme
-val clear_dark = Color(0xFFA05162)
-val dark_btn = Color(0xFF222427)
-
-// define your colors for dark theme
-val light_btn = Color(android.graphics.Color.parseColor("#E9F0F4"))
-val light_bg = Color(android.graphics.Color.parseColor("#F6F8F9"))
-val clear_light = Color(0xFFF1C8D1)
-
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
