1a74a6055e3aa9dff1a7d415ee2e7903e16218cf
Krzysztof Hofman <Krzysztof.Hofman@tomtom.com>
2024-08-15 14:05:17 +0200

    Fix TTS speak

--- app/src/main/java/com/gofer/speechtraining/screens/TrainingScreen.kt
+++ app/src/main/java/com/gofer/speechtraining/screens/TrainingScreen.kt
@@ -4,11 +4,13 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -22,9 +24,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.gofer.speechtraining.TrainingScreenLabel
+import com.gofer.speechtraining.src.main.model.TtsViewModel
 import com.gofer.speechtraining.ui.theme.Pink40
 import com.gofer.speechtraining.ui.theme.SpeechTrainingTheme
 
@@ -33,9 +37,11 @@ import com.gofer.speechtraining.ui.theme.SpeechTrainingTheme
 @Composable
 fun TrainingScreen(
   trainingTopicName: String,
-  phrases: List<String>
+  phrases: List<String>,
+  ttsViewModel: TtsViewModel = TtsViewModel()
 ) {
   val trainingPhrasesState = remember { phrases.toMutableStateList() }
+  val context = LocalContext.current
 
   Scaffold(
     topBar = {
@@ -60,7 +66,15 @@ fun TrainingScreen(
           },
           contentAlignment = Alignment.Center
         ) {
-          Text( text = phrase.trim('[', ']'))
+          Row(modifier = Modifier.fillMaxWidth()) {
+            val phraseText = phrase.trim('[', ']')
+            Text( text = phraseText)
+            Button(onClick = {
+              ttsViewModel.onSpeakTrainingPhrase(phraseText, context)
+            }) {
+              Text(text = "Speak")
+            }
+          }
         }
       }
     }
--- app/src/main/java/com/gofer/speechtraining/ui/theme/Theme.kt
+++ app/src/main/java/com/gofer/speechtraining/ui/theme/Theme.kt
@@ -9,6 +9,26 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
+//sealed class ThemeColors(
+//    val bacground: Color,
+//    val surafce: Color,
+//    val primary: Color,
+//    val text: Color
+//)  {
+//    object Night: ThemeColors(
+//        bacground = Color.Black,
+//        surafce = dark_btn,
+//        primary = clear_dark,
+//        text = Color.White
+//    )
+//    object Day: ThemeColors(
+//        bacground = light_bg,
+//        surafce = light_btn,
+//        primary = clear_light,
+//        text = Color.Black
+//    )
+//}
+
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
@@ -18,17 +38,7 @@ private val DarkColorScheme = darkColorScheme(
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    tertiary = Pink40,
 )
 
 @Composable
