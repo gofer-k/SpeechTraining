8784f025e9ef0683d5cb0872e3306fc571685d9e
Krzysztof Hofman <Krzysztof.Hofman@tomtom.com>
2024-08-16 20:07:34 +0200

    Move speaking items top training screen. Add speaking icon

--- app/src/main/java/com/gofer/speechtraining/TrainingScreenLabel.kt
+++ app/src/main/java/com/gofer/speechtraining/TrainingScreenLabel.kt
@@ -5,5 +5,10 @@ import androidx.annotation.StringRes
 enum class TrainingScreenLabel(@StringRes val title: Int)  {
   TrainingList(R.string.app_name),
   TrainingConfiguration(R.string.training_configuration),
-  TrainingContents(R.string.training_topic)
+  TrainingContents(R.string.training_topic),
+  TrainingPhraseSpaak(R.string.btn_speak),
+  TrainingPhraseSpeakIcon(R.drawable.ic_outline_volume_up_24_light),
+  TrainingPhraseSpeakDark(R.drawable.ic_outline_volume_up_24)
 }
+
+fun getTrainingSpeakIcon(isDarkTheme: Boolean) = if (isDarkTheme) R.drawable.ic_outline_volume_up_24 else R.drawable.ic_outline_volume_up_24_light
