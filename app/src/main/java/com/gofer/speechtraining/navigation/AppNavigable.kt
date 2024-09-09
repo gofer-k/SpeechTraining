febf681785ffc62b094d096209dcdd9b35cfea48
Krzysztof Hofman <Krzysztof.Hofman@tomtom.com>
2024-09-09 13:06:40 +0200

    Fix cycling between navigable views

--- app/src/main/java/com/gofer/speechtraining/navigation/AppNavigable.kt
+++ app/src/main/java/com/gofer/speechtraining/navigation/AppNavigable.kt
@@ -8,6 +8,8 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.gofer.speechtraining.TrainingScreenLabel
+import com.gofer.speechtraining.TrainingScreenLabel.TrainingAddPhrase
+import com.gofer.speechtraining.TrainingScreenLabel.TrainingList
 import com.gofer.speechtraining.screens.AddTrainingDataScreen
 import com.gofer.speechtraining.screens.TrainingConfigurationScreen
 import com.gofer.speechtraining.src.main.model.SpeechTrainingDataViewModel
@@ -15,40 +17,33 @@ import com.gofer.speechtraining.src.main.model.SpeechTrainingDataViewModel
 @Composable
 fun AppNavigation(viewModel: SpeechTrainingDataViewModel) {
   val navHostController = rememberNavController()
-  NavHost(navController = navHostController, startDestination = TrainingScreenLabel.TrainingList.name) {
-      composable(TrainingScreenLabel.TrainingList.name) {
+  NavHost(navController = navHostController, startDestination = TrainingList.name) {
+      composable(TrainingList.name) {
         HomeScreen(viewModel.getTrainingTopics(), navController = navHostController) }
-      composable(
-        "${TrainingScreenLabel.TrainingConfiguration.name}?topicId={topicId}&addPhrase={addPhrase}&phraseLang={phraseLang}",
-      arguments = listOf(
-        navArgument(name = "topicId") {
-          type = NavType.LongType
-          defaultValue = 0
-        },
-        navArgument(name = "addPhrase") {
-          type = NavType.StringType
-          defaultValue = ""
-        },
-        navArgument(name = "phraseLang") {
-          type = NavType.StringType
-          defaultValue = ""
-        }
-      )
-    )
-      { backStackEntry ->
-        // TODO: Add new phrase into view model as well refresh Topic phrases
+      composable("${TrainingScreenLabel.TrainingConfiguration.name}?topicId={topicId}",
+        // Navigable view with forward argument
+        arguments = listOf(
+          navArgument(name = "topicId") {
+            type = NavType.LongType
+            defaultValue = 0
+          }
+        )
+      ) { backStackEntry ->
         val navArgs = backStackEntry.arguments
         val topicId = navArgs?.getLong("topicId") ?: 0L
-        val addPhraseText = navArgs?.getString("addPhrase") ?: ""
-        val phraseLang = navArgs?.getString("phraseLang") ?: ""
 
+        // Navigable previous view (add phrase view)
+        val addPhraseText = backStackEntry.savedStateHandle.get<String>("addPhrase")
+        val phraseLang = backStackEntry.savedStateHandle.get<String>("phraseLang")
+
+        // TODO: Add new phrase into view model as well refresh Topic phrases
         viewModel.getTrainingTopic(topicId = topicId)?.let {
           TrainingConfigurationScreen(navController = navHostController,
             viewModel.getTrainingPhrases(topicId),
             it)
         }
     }
-    composable("${TrainingScreenLabel.TrainingAddPhrase.name}?topicId={topicId}",
+    composable("${TrainingAddPhrase.name}?topicId={topicId}",
       arguments =  listOf(
         navArgument(name = "topicId") {
           type = NavType.LongType
