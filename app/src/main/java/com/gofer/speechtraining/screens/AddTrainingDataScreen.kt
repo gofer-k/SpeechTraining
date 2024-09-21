d5d3f2e57d9341bb3822a1487a089dd6f5e0c573
Krzysztof Hofman <Krzysztof.Hofman@tomtom.com>
2024-09-21 17:57:49 +0200

    Refactor compose languages list

--- app/src/main/java/com/gofer/speechtraining/screens/AddTrainingDataScreen.kt
+++ app/src/main/java/com/gofer/speechtraining/screens/AddTrainingDataScreen.kt
@@ -3,17 +3,11 @@ package com.gofer.speechtraining.screens
 import android.annotation.SuppressLint
 import android.content.res.Configuration
 import android.util.Log
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -21,7 +15,6 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Build
-import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.material.icons.rounded.Done
 import androidx.compose.material3.BottomAppBar
@@ -42,7 +35,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
@@ -50,13 +42,13 @@ import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.gofer.speechtraining.Language
 import com.gofer.speechtraining.TrainingScreenLabel
 import com.gofer.speechtraining.src.main.model.Phrase
 import com.gofer.speechtraining.src.main.model.Topic
+import com.gofer.speechtraining.ui.compose.LanguageList
 import com.gofer.speechtraining.ui.theme.Purple40
 import com.gofer.speechtraining.ui.theme.SpeechTrainingTheme
 import java.util.Locale
@@ -174,69 +166,6 @@ fun AddTrainingDataScreen(
     }
   }
 }
-
-@Composable
-fun LanguageList(languages: List<Language>, onSelectedLanguage: (Locale) -> Unit) {
-  var isExtended by remember { mutableStateOf( false) }
-  val label = stringResource(TrainingScreenLabel.TrainingLanguage.title)
-  var selectedLanguage by remember { mutableStateOf(Language(label)) }
-  val textSize = 20.sp
-  val cornerShape = RoundedCornerShape(24.dp)
-  val itemHeight = 32.dp//48.dp
-  val textHorizontalPadding = 12.dp
-  val backGroundColder = if (isSystemInDarkTheme()) Color.DarkGray else Color.LightGray
-
-  Column(modifier = Modifier
-    .fillMaxWidth()
-    .padding(64.dp)) {
-    Row(
-      modifier = Modifier
-        .fillMaxWidth()
-        .height(itemHeight)
-        .background(
-          color = backGroundColder,
-          shape = cornerShape
-        )
-        .clickable {
-          isExtended = !isExtended
-          if (!isExtended)
-            onSelectedLanguage(selectedLanguage.locale)
-        },
-      verticalAlignment = Alignment.CenterVertically,
-      horizontalArrangement = Arrangement.SpaceBetween) {
-      Text(modifier = Modifier.padding(horizontal = textHorizontalPadding),
-        text = selectedLanguage.label,
-        fontSize = textSize)
-      Icon(modifier = Modifier.padding(end = textHorizontalPadding),
-        imageVector = Icons.Default.List,
-        contentDescription = null)
-    }
-
-    if (isExtended) {
-      for (lang in languages) {
-        Spacer(modifier = Modifier.height(2.dp))
-        Box(modifier = Modifier
-          .fillMaxWidth()
-          .background(
-            color = backGroundColder,
-            shape = cornerShape
-          )
-          .height(itemHeight)
-          .clickable {
-            selectedLanguage = lang
-            isExtended = !isExtended
-          }) {
-          Text(text = lang.label,
-            modifier = Modifier
-              .padding(horizontal = textHorizontalPadding)
-              .align(alignment = Alignment.Center),
-            fontSize = textSize,
-            color = if(isSystemInDarkTheme()) Color.LightGray else Color.DarkGray)
-          }
-        }
-      }
-    }
-  }
 
 @Preview(showBackground = true, name = "Dark Mode", uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
