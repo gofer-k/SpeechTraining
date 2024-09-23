89fb02b92b827022d92cf7eccf0ecfb5ba973a3c
Krzysztof Hofman <Krzysztof.Hofman@tomtom.com>
2024-09-23 19:40:12 +0200

    Add scroll indicator for the first visible phrase for training list

---
+++ app/src/main/java/com/gofer/speechtraining/ui/compose/LazyColumnScrollPosition.kt
@@ -0,0 +1,71 @@
+package com.gofer.speechtraining.ui.compose
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.gofer.speechtraining.src.main.model.Phrase
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LazyColumnScrollPosition(parentListState: LazyListState = rememberLazyListState(), listPhrases: List<Phrase>) {
+  val scope = rememberCoroutineScope()
+
+  Box(modifier = Modifier.fillMaxSize(),
+    contentAlignment = Alignment.CenterEnd) {
+    Column(modifier = Modifier
+      .padding(4.dp)
+      .align(Alignment.CenterEnd),
+      verticalArrangement = Arrangement.spacedBy(8.dp)) {
+      FloatingActionButton(
+        onClick = {
+          scope.launch {
+            parentListState.animateScrollToItem(10)
+          }
+        },
+        shape = RoundedCornerShape(32.dp),
+        contentColor = Color.DarkGray,
+        elevation = FloatingActionButtonDefaults.elevation()
+      ) {
+        val firstVisibleIndex = remember {derivedStateOf { parentListState.firstVisibleItemIndex } }
+        if (firstVisibleIndex.value >= 0) {
+          Text(
+            text = listPhrases.get(firstVisibleIndex.value).name.first().toString(),
+            fontWeight = FontWeight.Bold,
+            fontSize = 18.sp
+          )
+        }
+      }
+    }
+  }
+}
+
+
+@Preview(showBackground = true, name = "Dark Mode", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun LazyColumnScrollPositionPreview() {
+  val listState = rememberLazyListState( initialFirstVisibleItemIndex = 0 )
+  val phrases = listOf<Phrase>()
+  LazyColumnScrollPosition(listState, phrases)
+}
