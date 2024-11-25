e44f68efbc3431822c3ff48ee726a552ed44116f
Krzysztof Hofman <Krzysztof.Hofman@tomtom.com>
2024-11-25 20:20:48 +0100

    feat: add pronunciation to data model

--- app/src/main/java/com/gofer/speechtraining/src/main/model/Phrase.kt
+++ app/src/main/java/com/gofer/speechtraining/src/main/model/Phrase.kt
@@ -3,7 +3,7 @@ package com.gofer.speechtraining.src.main.model
 import java.util.Locale
 
 
-data class Phrase(val name: String = "", var isSelected: Boolean = false, val language: Locale = Locale.US) {
+data class Phrase(val name: String = "", val pron: String = "", var isSelected: Boolean = false, val language: Locale = Locale.US) {
   fun toggle() {
     isSelected = isSelected.not()
   }
