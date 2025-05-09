f81f94d556185f31fa6c690b89bdd40ac35d8c68
gofer-k <khofman250377@gmail.com>
2025-05-09 23:01:12 +0200

    chore: clean code

--- app/src/main/java/com/gofer/speechtraining/ui/TopBarTitle.kt
+++ app/src/main/java/com/gofer/speechtraining/ui/TopBarTitle.kt
@@ -5,10 +5,6 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
